54bc55c1bc4f984c96c87de86721d79f8aa9311f
Brandon Hale <bthaleproductions@gmail.com>
2021-03-09 19:55:25 -0500

    Added the first version of make-cfg.sh

---
+++ make-cfg.sh
@@ -0,0 +1,30 @@
+#!/bin/bash                                                                     
+
+    # Retropie Airplane Mode scripts; allows for disabling and enabling wireless.
+    # Copyright (C) 2021 Brandon Hale
+
+    # This program is free software: you can redistribute it and/or modify
+    # it under the terms of the GNU General Public License as published by
+    # the Free Software Foundation, either version 3 of the License, or
+    # (at your option) any later version.
+
+    # This program is distributed in the hope that it will be useful,
+    # but WITHOUT ANY WARRANTY; without even the implied warranty of
+    # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    # GNU General Public License for more details.
+
+    # You should have received a copy of the GNU General Public License
+    # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+for i in *.png; do                                                            
+    FILENAME="$(ls "$i" | sed 's/.png/.cfg/')"                         
+    echo "overlays = 1" >> $FILENAME
+    echo >> $FILENAME               
+    echo "overlay0_overlay = $i" >> $FILENAME 
+    echo >> $FILENAME               
+    echo "overlay0_full_screen = true" >> $FILENAME
+    echo >> $FILENAME               
+    echo "overlay0_descs = 0" >> $FILENAME
+done                                                                            
+                                                                                
+                                       
